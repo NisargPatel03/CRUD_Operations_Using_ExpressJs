41e9f95f118d82665afc5c1186b33225a8c91165
NisargPatel03 <kbnisargpatel001454@gmail.com>
2025-09-16 12:29:18 +0530

    Updated App.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
+const cors = require('cors')
 
 mongoose.connect('mongodb://127.0.0.1:27017/charusat')
 .then(()=> console.log("DB Connected"))
@@ -19,6 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
